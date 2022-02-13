7d2f6782c0d17cae532f37cb92291f29e0646928
Devon Wolfkiel <devonwolf@tutanota.com>
2022-02-12 22:06:04 -0800

    add index view tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
 
 # Create your tests here.
@@ -19,4 +20,62 @@ class QuestionModelsTests(TestCase):
 		test_time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
 		recent_question = Question(pub_date=test_time)
 		self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+	published_time = timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, pub_date=published_time)
+
+def get_latest_question_list(response):
+		return response.context['latest_question_list']
+class QuestionIndexViewTests(TestCase):
+	def get_index_response(self):
+		return self.client.get(reverse('polls:index'))
+	
+	def test_no_questions(self):
+		response = self.get_index_response()
 		
+		expected = []
+		actual = get_latest_question_list(response=response)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(actual, expected)
+
+	def test_past_question(self):
+		question = create_question(question_text="Is this a past question?", days=-30)
+		response = self.get_index_response()
+
+		expected = [question]
+		actual = get_latest_question_list(response=response)
+		
+		self.assertQuerysetEqual(actual, expected)
+
+	def test_future_question(self):
+		create_question(question_text="Is this a future question?", days=30)
+		response = self.get_index_response()
+
+		expected = []
+		actual = get_latest_question_list(response=response)
+
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(actual, expected)
+	
+	def test_past_and_future_question(self):
+		past_question = create_question(question_text="Is this a past question?", days=-30)
+		create_question(question_text="Is this a future question?", days=30)
+		response = self.get_index_response()
+
+		expected = [past_question]
+		actual = get_latest_question_list(response=response)
+
+		self.assertQuerysetEqual(actual, expected)
+
+	def test_two_past_questions(self):
+		question1 = create_question(question_text="Past question 1?", days=-30)
+		question2 = create_question(question_text="Past question 2?", days=-5)
+		response = self.get_index_response()
+
+		expected = [question2, question1]
+		actual = get_latest_question_list(response=response)
+
+		self.assertQuerysetEqual(actual, expected)
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 # Create your views here.
@@ -9,7 +10,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]\
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
